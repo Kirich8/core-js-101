5daad4d199737b69f1189c9179ff0c9d45c87a0c
Maxim Kirichenko <kirichenkomaxim@icloud.com>
2023-06-17 15:19:41 +0300

    feat: add solutions functions tasks

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -151,8 +151,14 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    logFunc(`${func.name}(${args.map((item) => JSON.stringify(item))}) starts`);
+    const result = func(...args);
+    logFunc(`${func.name}(${args.map((item) => JSON.stringify(item))}) ends`);
+
+    return result;
+  };
 }
 
 
